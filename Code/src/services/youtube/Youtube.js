75ca7b34d03cc30054146699527db1a869bc1f8b
SaadRegal <saadrg3@gmail.com>
2019-02-17 20:56:03 +0100

    user is able to select more than 50 videos

--- Code/src/services/youtube/Youtube.js
+++ Code/src/services/youtube/Youtube.js
@@ -3,24 +3,39 @@ import {appConfig} from '../../config';
 import {VideoClass} from '../../models/video.class';
 
 const axios = Axios.create({
-  baseURL: appConfig.getYoutubeEndPoint('videos')
+    baseURL: appConfig.getYoutubeEndPoint('videos')
 });
 
 export class YoutubeService {
-  getTrendingVideos(videosPerPage = appConfig.maxVideosToLoad) {
-    const params = {
-      part: appConfig.partsToLoad,
-      chart: appConfig.chart,
-      videoCategoryId: appConfig.defaultCategoryId,
-      regionCode: appConfig.defaultRegion,
-      maxResults: videosPerPage,
-      key: appConfig.youtubeApiKey
+    nextPageToken = '';
+    makeRequest= async (params)=>{
+        return axios.get('/', {params}).then(({ data }) => {
+            const { items, nextPageToken } = data;
+            this.nextPageToken = nextPageToken;
+
+            return items
+                .map((item) => new VideoClass(item));
+        }).catch((err) => err);
     };
 
-    return axios.get('/', {params}).then((res) => {
-      return res.data.items
-        .map((item) => new VideoClass(item))
-        .filter((item) => item.id !== '');
-    }).catch((err) => err);
-  }
+    getTrendingVideos=async(videosPerPage = appConfig.maxVideosToLoad)=> {
+        let videos = [];
+        let getParams = {
+            part: appConfig.partsToLoad,
+            chart: appConfig.chart,
+            videoCategoryId: appConfig.defaultCategoryId,
+            regionCode: appConfig.defaultRegion,
+            maxResults: Math.min(videosPerPage, 50),
+            key: appConfig.youtubeApiKey,
+            pageToken: this.nextPageToken
+        };
+
+        while (videos.length < videosPerPage) {
+            getParams.maxResults = Math.min(videosPerPage - videos.length, 50);
+            getParams.pageToken = this.nextPageToken;
+            videos = videos.concat(await this.makeRequest(getParams));
+        }
+
+        return videos;
+    }
 }
